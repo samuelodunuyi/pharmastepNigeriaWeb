8461ca9929a1f27ea77f2020c481cbc2c210b423
Samuel Abayomi Odunuyi <samuel.odunuyi@codeware.com.ng>
2023-05-29 22:57:42 +0100

    dd

--- src/components/topHeader.vue
+++ src/components/topHeader.vue
@@ -35,45 +35,45 @@
         </div>
       </nav>
     </div>
-    <div id="carouselExampleCrossfade" class="carousel slide carousel-fade" data-mdb-ride="carousel">
+    <div id="carouselExampleCrossfade" class="carousel slide carousel-fade carousel-dark" data-bs-ride="carousel">
   <div class="carousel-indicators">
     <button
       type="button"
-      data-mdb-target="#carouselExampleCrossfade"
-      data-mdb-slide-to="0"
+      data-bs-target="#carouselExampleCrossfade"
+      data-bs-slide-to="0"
       class="active"
       aria-current="true"
       aria-label="Slide 1"
     ></button>
     <button
       type="button"
-      data-mdb-target="#carouselExampleCrossfade"
-      data-mdb-slide-to="1"
+      data-bs-target="#carouselExampleCrossfade"
+      data-bs-slide-to="1"
       aria-label="Slide 2"
-    ></button>
-    <button
-      type="button"
-      data-mdb-target="#carouselExampleCrossfade"
-      data-mdb-slide-to="2"
-      aria-label="Slide 3"
     ></button>
   </div>
   <div class="carousel-inner">
     <div class="carousel-item active">
-      <img src="https://mdbcdn.b-cdn.net/img/new/slides/041.webp" class="d-block w-100" alt="Wild Landscape"/>
+      <video 
+      src="https://firebasestorage.googleapis.com/v0/b/shopper-56289.appspot.com/o/pharmastep.mp4?alt=media&token=7e67bb6f-68e4-43de-8d8f-828018226315" autoplay></video>
+      <div class="carousel-caption d-none d-md-block">
+        <h5>First slide label</h5>
+        <p>Nulla vitae elit libero, a pharetra augue mollis interdum.</p>
+      </div>
     </div>
     <div class="carousel-item">
       <img src="../components/images/hero_1.jpg" class="d-block w-100" alt="Camera"/>
-    </div>
-    <div class="carousel-item">
-      <img src="https://mdbcdn.b-cdn.net/img/new/slides/043.webp" class="d-block w-100" alt="Exotic Fruits"/>
+      <div class="carousel-caption d-none d-md-block">
+        <h5>Second slide label</h5>
+        <p>Praesent commodo cursus magna, vel scelerisque nisl consectetur.</p>
+      </div>
     </div>
   </div>
-  <button class="carousel-control-prev" type="button" data-mdb-target="#carouselExampleCrossfade" data-mdb-slide="prev">
+  <button class="carousel-control-prev" type="button" data-bs-target="#carouselExampleCrossfade" data-bs-slide="prev">
     <span class="carousel-control-prev-icon" aria-hidden="true"></span>
     <span class="visually-hidden">Previous</span>
   </button>
-  <button class="carousel-control-next" type="button" data-mdb-target="#carouselExampleCrossfade" data-mdb-slide="next">
+  <button class="carousel-control-next" type="button" data-bs-target="#carouselExampleCrossfade" data-bs-slide="next">
     <span class="carousel-control-next-icon" aria-hidden="true"></span>
     <span class="visually-hidden">Next</span>
   </button>
